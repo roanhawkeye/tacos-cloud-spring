5d3ca6f7621c26b2e8a625b384d549881ec4744c
Daniel Martinez <roanhawkeye@gmail.com>
2022-08-20 17:15:05 -0500

    Add mock post method to order controller and add template for order form

--- src/main/java/sia/tacocloud/web/OrderController.java
+++ src/main/java/sia/tacocloud/web/OrderController.java
@@ -2,10 +2,13 @@ package sia.tacocloud.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
+import sia.tacocloud.TacoOrder;
 
 @Slf4j
 @Controller
@@ -18,5 +21,12 @@ public class OrderController {
     return "orderForm";
   }
 
+  @PostMapping
+  public String processOrder(TacoOrder order, SessionStatus sessionStatus){
+    log.info("Order Submitted: {}", order);
+    sessionStatus.setComplete();
+
+    return "redirect:/";
+  }
 
 }
