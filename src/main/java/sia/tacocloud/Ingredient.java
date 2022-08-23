7dc091a28fa47821f83d64a197a1b0a60eb1e01a
Daniel Martinez <roanhawkeye@gmail.com>
2022-08-23 14:41:29 -0500

    Revert "Refactor to use Spring JPA with Cassandra db"
    This reverts commit 730e955ca1365196c417fbc8888bf4c0dc3a4ff7.

--- src/main/java/sia/tacocloud/Ingredient.java
+++ src/main/java/sia/tacocloud/Ingredient.java
@@ -1,7 +1,7 @@
 package sia.tacocloud;
 
-import org.springframework.data.cassandra.core.mapping.PrimaryKey;
-import org.springframework.data.cassandra.core.mapping.Table;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -9,12 +9,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
-@Table("ingredients")
 public class Ingredient{
   
-  @PrimaryKey
+  @Id
   private final String id;
   private final String name;
   private final Type type;
--- src/main/java/sia/tacocloud/TacoOrder.java
+++ src/main/java/sia/tacocloud/TacoOrder.java
@@ -4,29 +4,28 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.CreditCardNumber;
-import org.springframework.data.cassandra.core.mapping.Column;
-import org.springframework.data.cassandra.core.mapping.PrimaryKey;
-import org.springframework.data.cassandra.core.mapping.Table;
-
-import com.datastax.oss.driver.api.core.uuid.Uuids;
 
 import lombok.Data;
 
 @Data
-@Table("orders")
+@Entity
 public class TacoOrder implements Serializable {
   
   private static final long serialVersionUID = 1L;
 
-  @PrimaryKey
-  private UUID id = Uuids.timeBased();
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
   private Date placeAt;
 
@@ -54,11 +53,10 @@ public class TacoOrder implements Serializable {
   @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
   private String ccCVV;
  
-  @Column("tacos")
-  private List<TacoUDT> tacos = new ArrayList<>();
+  private List<Taco> tacos = new ArrayList<>();
  
   public void addTaco(Taco taco) {
-    this.tacos.add(TacoUDRUtils.tacoUDT(taco));
+    this.tacos.add(taco);
   }
 
 }
