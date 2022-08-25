7ff46b404b80355d47d84d7db5b07dd848cefb3d
Daniel Martinez <roanhawkeye@gmail.com>
2022-08-25 18:35:54 -0500

    Add login page and authentication configuration

--- src/main/java/sia/tacocloud/security/SecurityConfig.java
+++ src/main/java/sia/tacocloud/security/SecurityConfig.java
@@ -2,10 +2,12 @@ package sia.tacocloud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import sia.tacocloud.User;
 import sia.tacocloud.data.UserRepository;
@@ -26,5 +28,21 @@ public class SecurityConfig {
 
       throw new UsernameNotFoundException("User '" + username + "' not found");
     };
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    return http
+      .authorizeRequests()
+        .antMatchers("/design", "/orders").hasRole("USER")
+        .antMatchers("/", "/**").permitAll()
+
+        .and()
+          .formLogin()
+            .loginPage("/login")
+            .defaultSuccessUrl("/design")
+
+        .and()
+        .build();
   }
 }
