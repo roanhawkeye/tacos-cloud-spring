74ebca6cc79978331fac9ac05c1ebc29b680f04f
Daniel Martinez <roanhawkeye@gmail.com>
2022-09-05 18:34:03 -0500

    Add some methods to Taco Controller

--- src/main/java/sia/tacocloud/web/api/TacoController.java
+++ src/main/java/sia/tacocloud/web/api/TacoController.java
@@ -1,10 +1,19 @@
 package sia.tacocloud.web.api;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import sia.tacocloud.Taco;
@@ -12,7 +21,7 @@ import sia.tacocloud.data.TacoRepository;
 
 @RestController
 @RequestMapping(path = "/api/tacos", produces = "application/json")
-@CrossOrigin(origins = "http://localhost:8181")
+@CrossOrigin(origins = "http://localhost:8081")
 public class TacoController {
   private TacoRepository tacoRepo;
 
@@ -26,5 +35,26 @@ public class TacoController {
     return tacoRepo.findAll(page).getContent();
   }
 
+  @GetMapping("/{id}")
+  public Optional<Taco> tacoById(@PathVariable("id") Long id){
+    return tacoRepo.findById(id);
+  }
+
+  @PostMapping(consumes = "application/json")
+  @ResponseStatus(HttpStatus.CREATED)
+  public Taco postTaco(@RequestBody Taco taco){
+    return tacoRepo.save(taco);
+  }
+
+  @PutMapping(path = "/{id}", consumes = "application/json")
+  public Taco putTaco(@PathVariable("id") Long id, @RequestBody Taco taco){
+    return tacoRepo.save(taco);
+  }
+
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteTaco(@PathVariable("id") Long id) {
+    tacoRepo.deleteById(id);
+  }
 
 }
